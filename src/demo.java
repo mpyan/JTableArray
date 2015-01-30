4894cb85d36ae51e111079d4835b69a8f80ccee3
MY <MY@MYs-MacBook-Pro.local>
2015-01-29 17:14:10 -0800

    create TableArray class and a demo class

---
+++ src/demo.java
@@ -0,0 +1,27 @@
+
+public class demo {
+
+	public static void main(String[] args) {
+		System.out.println("Demo");
+		TableArray demoTable = new TableArray(6); // 6 column TableArray
+		
+		demoTable.pushRow(1, 2, 3, 4, 5, 6);
+		demoTable.pushRow(7, 8, 9, 10, 11, 12);
+		demoTable.pushRow(6, 5, 4, 2, 2, 1);
+		demoTable.pushRow(1); // Will do nothing, not enough columns
+		demoTable.print();
+		
+		System.out.println("Sorted:");
+		demoTable.sort(5);
+		demoTable.print();
+		
+		System.out.println(""+demoTable.getItem(0, 2));
+		demoTable.setItem(0, 2, 100);
+		System.out.println(""+demoTable.getItem(0, 2));
+		
+		demoTable.print();
+		
+		System.out.println("done");
+	}
+
+}
